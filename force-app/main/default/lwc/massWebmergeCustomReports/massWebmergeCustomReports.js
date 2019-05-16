19a08be4c62f3d5d0bb582a8eb02407d6e3f2d4a
Dalton Curtin <dcurtin@midland.local>
2019-05-16 17:05:21 -0400

    Working Code, need to clean it up and beautify the interface

--- force-app/main/default/lwc/massWebmergeCustomReports/massWebmergeCustomReports.js
+++ force-app/main/default/lwc/massWebmergeCustomReports/massWebmergeCustomReports.js
@@ -1,3 +1,75 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import queryValue from '@salesforce/apex/massWebmergeCustomReportsHelper.queryValue';
+import getQueries from '@salesforce/apex/massWebmergeCustomReportsHelper.getQueries';
+import doWebmerge from '@salesforce/apex/massWebmergeCustomReportsHelper.doWebmerge';
 
-export default class MassWebmergeCustomReports extends LightningElement {}
+export default class MassWebmergeCustomReports extends LightningElement 
+{
+    @track soqls=null;//a list of possible soql queries
+    @track statusMessage;
+    @track relatedParties;
+    @track soqlOptionsList;
+    soqlOption = null;
+
+    get soqlOptions(){
+        return this.soqlOptionsList;
+    }
+    constructor()
+    {
+        super();
+        this.statusMessage = "Welcome, please choose a query.";
+        this.getSoqlQueries();
+    }
+
+    queryGenericCall()
+    {
+        if(this.soqlOption == null)
+            {
+                console.log("Fail Wale");
+                return;
+            }
+        this.queryValueCall(this.soqlOption);
+    }
+
+    mergeRecords()
+    {
+        var recordIds = [];
+        console.log(this.soqls[this.soqlOption].webmergeMappingName);
+        this.relatedParties.forEach(record => {recordIds.push(record.Id)});
+        console.log(recordIds);
+        doWebmerge({recordIds: recordIds, mappingName: this.soqls[this.soqlOption].webmergeMappingName})
+    }
+
+    queryValueCall(soqlName)
+    {
+        console.log(this.soqls[soqlName]);
+        queryValue({soqlQuery: this.soqls[soqlName]}).then(function(result)
+        {
+            console.log(result);
+            this.relatedParties=result;
+        }.bind(this))
+    }
+
+    getSoqlQueries()
+    {
+        getQueries().then(function(result)
+        {
+            this.soqls=result.queries;
+            this.getSoqlOptions(result.queries);
+        }.bind(this));
+    }
+
+    getSoqlOptions(availableQueries)
+    {
+        var values = [];
+        Object.keys(availableQueries).forEach(query => {
+            values.push({label: query, value: query})
+        });
+        this.soqlOptionsList = values;
+    }
+
+    setSoqlOption(event)
+    {
+        this.soqlOption = event.detail.value;
+    }
+}
