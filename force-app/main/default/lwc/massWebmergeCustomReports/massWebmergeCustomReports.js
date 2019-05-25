e608aec7127c01c9137658e577faad9c661c4a1d
Dalton Curtin <dcurtin@midland.local>
2019-05-25 10:51:29 -0400

    sort of working code, Can't use child elements in data-table. Need to just use name or only top level fields.

--- force-app/main/default/lwc/massWebmergeCustomReports/massWebmergeCustomReports.js
+++ force-app/main/default/lwc/massWebmergeCustomReports/massWebmergeCustomReports.js
@@ -1,104 +1,158 @@
 import { LightningElement, track } from 'lwc';
 import queryValue from '@salesforce/apex/massWebmergeCustomReportsHelper.queryValue';
-import getQueries from '@salesforce/apex/massWebmergeCustomReportsHelper.getQueries';
+import getQueriesFromStaticResource from '@salesforce/apex/massWebmergeCustomReportsHelper.getQueriesFromStaticResource';
 import doWebmerge from '@salesforce/apex/massWebmergeCustomReportsHelper.doWebmerge';
+
 
 export default class MassWebmergeCustomReports extends LightningElement 
 {
-    @track soqls=null;//a list of possible soql queries
     @track statusMessage;
     @track relatedParties;
-    @track soqlOptionsList;
-    soqlOption = null;
+    @track soqlOptionNamesList;
+    @track tableLoadingState = true;
 
-    get soqlOptions(){
-        return this.soqlOptionsList;
+    availableSoqlQueries=null;//a list of possible soql queries
+
+    @track tableColumnLabels = [
+        { label: 'Name', fieldName: 'name' },
+        { label: 'account', fieldName: 'Account__r.Id' }
+    ];
+
+    selectedSoqlOptionName = null;
+    selectedRows = [];
+    
+    /*
+        provide available 
+    */
+    get soqlOptionNames(){
+        return this.soqlOptionNamesList;
     }
     constructor()
     {
         super();
         this.statusMessage = "Welcome, please choose a query.";
-        this.getSoqlQueries();
+        this.getSoqlQueryOptions();
     }
 
-    toggleCheckBoxes(event)
+    getSelectedRecords(event)
     {
-        //console.log(event.target.checked);
-        if(event.target.checked)
-        {
-            this.checkAll();
-        }else
-        {
-            this.uncheckAll();
-        }
-    }
-
-    uncheckAll()
-    {
-        //uncheck all boxex
-        console.log('uncheck all');
-    }
-    checkAll()
-    {
-        //check all boxex
-        console.log('check all');
+        this.selectedRows = event.detail.selectedRows;
     }
 
     queryGenericCall()
     {
-        if(this.soqlOption == null)
+        if(this.selectedSoqlOptionName == null)
             {
                 this.failWhale();
                 return;
             }
-        this.queryValueCall(this.soqlOption);
+        this.queryValueCall(this.selectedSoqlOptionName);
     }
 
     mergeRecords()
     {
         var recordIds = [];
-        console.log(this.soqls[this.soqlOption].webmergeMappingName);
-        this.relatedParties.forEach(record => {recordIds.push(record.Id)});
+        console.log(this.availableSoqlQueries[this.selectedSoqlOptionName].webmergeMappingName);
+        this.selectedRows.forEach(record => {recordIds.push(record.Id)});
         console.log(recordIds);
-        doWebmerge({recordIds: recordIds, mappingName: this.soqls[this.soqlOption].webmergeMappingName})
+        doWebmerge({recordIds: recordIds, mappingName: this.availableSoqlQueries[this.selectedSoqlOptionName].webmergeMappingName})
     }
 
-    queryValueCall(soqlName)
+    queryValueCall(selectedSoqlOptionName)
     {
-        console.log(this.soqls[soqlName]);
-        queryValue({soqlQuery: this.soqls[soqlName]}).then(function(result)
+        console.log(this.availableSoqlQueries[selectedSoqlOptionName]);
+        queryValue({soqlQuery: this.availableSoqlQueries[selectedSoqlOptionName]}).then(function(result)
         {
-            console.log(result);
+            console.log(result[0].Account__r.Id);
             this.relatedParties=result;
+            this.tableLoadingState=false;
         }.bind(this))
     }
 
-    getSoqlQueries()
+    getSoqlQueryOptions()
     {
-        getQueries().then(function(result)
+        getQueriesFromStaticResource().then(function(result)
         {
-            this.soqls=result.queries;
-            this.getSoqlOptions(result.queries);
+            this.availableSoqlQueries=result.queries;
+            this.soqlOptionNamesList = this.getSoqlOptionNames(result.queries);
         }.bind(this));
     }
 
-    getSoqlOptions(availableQueries)
+    getTableColumnLabels(selectedSoqlOptionName)
     {
-        var values = [];
-        Object.keys(availableQueries).forEach(query => {
-            values.push({label: query, value: query})
-        });
-        this.soqlOptionsList = values;
+        var queryParts = this.breakDownQueryStringRemoveSelect(selectedSoqlOptionName)
+        var columnNames = this.parseOutColumnNames(queryParts);
+        return this.generateFormattedColumnNames(columnNames);
     }
 
-    setSoqlOption(event)
+    generateFormattedColumnNames(columnNames)
     {
-        this.soqlOption = event.detail.value;
+        var columnNamesFormatted = [];
+
+        columnNames.forEach(name=>
+            {
+                columnNamesFormatted.push({ label: name, fieldName: name })
+            })
+            console.log(columnNamesFormatted);
+            return columnNamesFormatted;
+    }
+
+    breakDownQueryStringRemoveSelect(selectedSoqlOptionName)
+    {
+        var queryString =  this.availableSoqlQueries[selectedSoqlOptionName].query;
+        var queryParts = queryString.split(' ');
+        return queryParts.slice(1); //return array with 'select' removed
+    }
+
+    parseOutColumnNames(queryParts)
+    {
+        var columns = [];
+        var partsCount = queryParts.length;
+        var index;
+        var valueAtIndex;
+        var valueAtIndexCleaned;
+
+        for(index=0; index < partsCount; ++index)
+        {
+            valueAtIndex = queryParts[index];
+            valueAtIndexCleaned = valueAtIndex.replace(',', '');
+
+
+            if(valueAtIndexCleaned.toLowerCase() === 'from')
+                break;
+
+            columns.push(valueAtIndexCleaned);
+        }
+        return columns;
+    }
+
+    getSoqlOptionNames(availableQueries)
+    {
+        console.log("getting options");
+        var soqlQueryNames = [];
+
+        if(availableQueries == null)
+        {
+            return soqlQueryNames;
+        }
+        
+        Object.keys(availableQueries).forEach(query => {
+            soqlQueryNames.push({label: query, value: query})
+        });
+        console.log(soqlQueryNames);
+        return soqlQueryNames;
+    }
+
+    setselectedSoqlOptionName(event)
+    {
+        this.selectedSoqlOptionName = event.detail.value;
+        //this.tableColumnLabels = this.getTableColumnLabels(this.selectedSoqlOptionName);
     }
 
     failWhale()
     {
-        var failWhaleString = ` FAIL WHALE!
+        var failWhaleString = ` 
+            FAIL WHALE!
 
         W     W      W        
         W        W  W     W    
